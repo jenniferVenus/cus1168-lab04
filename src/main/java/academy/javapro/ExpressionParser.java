d3ea00d0c2e97ee07b0f3c3893c4020cc7a21b0f
Jennifer Venus <jennifer.venus22@my.stjohns.edu>
2025-02-23 23:32:20 -0500

    Finished homework

--- src/main/java/academy/javapro/ExpressionParser.java
+++ src/main/java/academy/javapro/ExpressionParser.java
@@ -11,45 +11,77 @@ class ExpressionParser {
 
     // expr → expr + term
     public double parseExpression() {
-        // TODO: Get the leftmost term value by calling parseTerm()
-        // TODO: While we haven't reached the end of input and current char is '+'
-        // TODO: Increment position to skip the '+' operator
-        // TODO: Get the next term on the right
-        // TODO: Add the right term to our running total
-        // TODO: Return the final value
-        throw new UnsupportedOperationException("Implement parseExpression");
+
+        double leftmostTerm = parseTerm();
+        double total = leftmostTerm;
+        
+       while(position < input.length() && input.charAt(position) == '+')
+       {
+            position++;
+            double rightTerm = parseTerm();
+            total += rightTerm;
+       }
+       
+        return total;
+       
     }
 
     // term → term * factor
     private double parseTerm() {
-        // TODO: Get the leftmost factor value by calling parseFactor()
-        // TODO: While we haven't reached the end of input and current char is '*'
-        // TODO: Increment position to skip the '*' operator
-        // TODO: Get the next factor on the right
-        // TODO: Multiply the right factor with our running total
-        // TODO: Return the final value
-        throw new UnsupportedOperationException("Implement parseTerm");
+       
+        double leftmostTerm = parseFactor();
+        double total = leftmostTerm;
+
+        while(position < input.length() && input.charAt(position) == '*')
+        {
+            position++;
+            double rightTerm = parseFactor();
+            total *= rightTerm;
+        }
+
+        return total;
     }
 
     // factor → ( expr )
     private double parseFactor() {
-        // TODO: Check if we have an opening parenthesis and haven't reached end of input
-        // TODO: Increment position to skip the opening parenthesis
-        // TODO: Parse the expression inside the parentheses
-        // TODO: Increment position to skip the closing parenthesis
-        // TODO: Return the result of the parenthesized expression
-        // TODO: If no parentheses, parse and return a number
-        throw new UnsupportedOperationException("Implement parseFactor");
+        
+        if(position < input.length() && input.charAt(position) == '(')
+        {
+            position++;
+            double expression = parseExpression();
+            
+            if(position < input.length() && input.charAt(position) == ')')
+            {
+                position++;
+                return expression;
+            }
+            else
+                throw new IllegalArgumentException("Uncompleted set of parentheses");
+        }
+
+        else
+             return parseNumber();
+
     }
 
     // Parse a numeric value
     private double parseNumber() {
-        // TODO: Create a StringBuilder to collect digits
-        // TODO: While we haven't reached the end and current char is digit or decimal point
-        // TODO: Append the current digit to our number string
-        // TODO: Move to next character
-        // TODO: Convert the string of digits to a double and return it
-        throw new UnsupportedOperationException("Implement parseNumber");
+        
+        StringBuilder digits = new StringBuilder();
+
+        while(position < input.length() && (input.charAt(position) == '.' || Character.isDigit(input.charAt(position))))
+        {
+            digits.append(input.charAt(position));
+            position++;
+        }
+
+        if(digits.length() == 0)
+        {
+            throw new IllegalArgumentException("Invalid number at position " + position);
+        }
+
+        return Double.parseDouble(digits.toString());
+        
     }
 
     public static void main(String[] args) {
@@ -75,3 +107,4 @@ class ExpressionParser {
         }
     }
 }
+
